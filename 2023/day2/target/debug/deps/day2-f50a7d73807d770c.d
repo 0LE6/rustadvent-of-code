4f392c65f4b4496535c28b0dadb86c1854735137
0LE6 <135649528+0LE6@users.noreply.github.com>
2024-12-29 23:15:49 +0100

    Delete 2023/day2/target directory

--- 2023/day2/target/debug/deps/day2-f50a7d73807d770c.d
+++
@@ -1,5 +0,0 @@
-/home/slimbook/projects/rust/advent-of-code/2023/day2/target/debug/deps/libday2-f50a7d73807d770c.rmeta: src/main.rs
-
-/home/slimbook/projects/rust/advent-of-code/2023/day2/target/debug/deps/day2-f50a7d73807d770c.d: src/main.rs
-
-src/main.rs:
